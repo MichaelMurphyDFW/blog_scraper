d81aa9797920fa937503c48369676755c5c26bd7
Michael Murphy <paintballmjm@gmail.com>
2020-01-15 15:18:21 -0600

    De-duplicated requests from pulling next_page and contents separately.

--- blog_scraping.py
+++ blog_scraping.py
@@ -1,58 +1,60 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-# from page_finder import next_page
+from time import sleep
+from random import uniform
 
-csv_file = "colt/scraping/blog_scraping/scraping_proj.csv"
-
-def next_page(base_url, blog_url):
-    try:
-        response = requests.get(blog_url).text
-        soup = BeautifulSoup(response, "html.parser")
-        next_page_finder = soup.find(class_="next").find("a")['href']
-        return f"{base_url}{next_page_finder}"
-    except AttributeError:
-        return False
+csv_file = "scraping_proj.csv"
 
 with open(csv_file, "w") as f:
+
     # Establish base blog url and csv headers
     base_url = "https://www.rithmschool.com"
     blog_url = "https://www.rithmschool.com/blog"
     headers = ['Title', 'Date', 'URL']
 
-    #Write CSV headers
+    # Write CSV headers
     csv_writer = csv.DictWriter(f, fieldnames=headers)
     csv_writer.writeheader()
 
-    #While a next page exists, scrape articles
-    while next_page(base_url, blog_url):
-        print(f'searching page {blog_url}')
+    # Loop through blog, breaking out once articles on last page have been scraped.
+    while True:
+        # Pull HTML with Beautiful Soup
         response = requests.get(blog_url)
         soup = BeautifulSoup(response.text, "html.parser")
-        articles = soup.find_all("article")
 
-        #Loop through each article tag on page, collecting url,date,title
+        # last_page flag determines if we are on the last available page
+        last_page = False
+
+        # If we are on the last page, set last_page to True
+        try:
+            next_page = soup.find(class_="next").find("a")['href']
+        except AttributeError:
+            last_page = True
+
+        # Find articles on page
+        articles = soup.find_all("article")
+        print(f'Searching page {blog_url}')
+
+        # Loop through each article tag on page, collecting url,date,title
         for article in articles:
             a_tag = article.find("a")
             title = a_tag.get_text()
             url = f"https://www.rithmschool.com{a_tag.attrs['href']}"
             date = article.find("time")['datetime']
-            csv_writer.writerow({'Title':title, 'Date': date, 'URL':url})
+            csv_writer.writerow({'Title': title, 'Date': date, 'URL': url})
 
-        #Update blog_url with url of next blog page
-        blog_url = next_page(base_url,blog_url)
+        # After collecting articles, if we are on the last page, break out.
+        # Else, update blog_url with next page.
 
+        if last_page:
+            print("Last page reached. Terminating.")
+            break
+        else:
+            # Update blog_url with url of next blog page
+            blog_url = f"{base_url}{next_page}"
 
-
-
-
-# with open(csv_file, "w") as f:
-#     headers = ['Title', 'Date', 'URL']
-#     csv_writer = csv.DictWriter(f, fieldnames=headers)
-#     csv_writer.writeheader()
-#     for article in articles:
-#         a_tag = article.find("a")
-#         title = a_tag.get_text()
-#         url = f"https://www.rithmschool.com{a_tag.attrs['href']}"
-#         date = article.find("time")['datetime']
-#         csv_writer.writerow({'Title':title, 'Date': date, 'URL':url})
+            # Pause for 5-7s before next page request
+            sleep_time = uniform(5.0, 7.0)
+            print(f"Sleeping for {round(sleep_time,2)} seconds")
+            sleep(sleep_time)
